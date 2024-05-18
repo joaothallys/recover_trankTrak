1498535ec5f6fde896497f91c0000765b6201b91
joaothallys <joaothallys>
2024-05-18 15:14:15 -0300

    trocar senha simples

--- javascript/recuperar-senha.js
+++ javascript/recuperar-senha.js
@@ -1,10 +1,8 @@
 document.getElementById('recoveryForm').addEventListener('submit', async function(event) {
     event.preventDefault();
     
-    
     const email = document.getElementById('email').value;
     const novaSenha = document.getElementById('novaSenha').value;
-    
     
     const requestBody = {
         email_usuario: email,
@@ -12,7 +10,6 @@ document.getElementById('recoveryForm').addEventListener('submit', async functio
     };
     
     try {
-        
         const response = await fetch('https://node-deploy-api-d20r.onrender.com/usuario/trocarsenha', {
             method: 'PUT',
             headers: {
@@ -22,18 +19,15 @@ document.getElementById('recoveryForm').addEventListener('submit', async functio
         });
         
         if (response.ok) {
-            
             document.getElementById('mensagemSucesso').textContent = 'Senha alterada com sucesso!';
             document.getElementById('mensagemSucesso').classList.remove('hidden');
             document.getElementById('mensagemErro').classList.add('hidden');
         } else {
-            
             document.getElementById('mensagemErro').textContent = 'Erro ao alterar a senha. Por favor, tente novamente.';
             document.getElementById('mensagemErro').classList.remove('hidden');
             document.getElementById('mensagemSucesso').classList.add('hidden');
         }
     } catch (error) {
-        
         console.error('Erro ao enviar solicitação:', error);
         document.getElementById('mensagemErro').textContent = 'Erro ao enviar solicitação. Por favor, tente novamente mais tarde.';
         document.getElementById('mensagemErro').classList.remove('hidden');
