780e6cd51f6f819b91795530801eb898274daea5
joaothallys <joaothallys>
2024-05-18 15:05:32 -0300

    trocar senha simples

--- javascript/recuperar-senha.js
+++ javascript/recuperar-senha.js
@@ -1,45 +1,42 @@
-const nodemailer = require('nodemailer');
-
-// Configurar o transporte de e-mail
-const transporter = nodemailer.createTransport({
-    service: 'smtp.hostinger.com',
-    port: 587,
-    auth: {
-        user: 'joao.thallys@chatshub.com.br',
-        pass: 'iu36O!jBufFm#qTB~u+hH$+VqzIizzae'
-    }
-});
-
-// Função para enviar e-mail de recuperação de senha
-function enviarEmailRecuperacaoSenha(email) {
-    const linkRecuperacaoSenha = `http://seusite.com/recuperar-senha?token=`;
-    const mailOptions = {
-        from: 'joao.thallys@chatshub.com.br',
-        to: email,
-        subject: 'Recuperação de Senha',
-        html: `<p>Você solicitou a recuperação de senha. Clique <a href="${linkRecuperacaoSenha}">aqui</a> para redefinir sua senha.</p>`
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.error('Erro ao enviar e-mail:', error);
-            // Exibir mensagem de erro ao usuário
-            alert('Ocorreu um erro ao enviar o e-mail de recuperação de senha. Por favor, tente novamente mais tarde.');
-        } else {
-            console.log('E-mail enviado:', info.response);
-            // Exibir mensagem de sucesso ao usuário
-            alert('Um e-mail de recuperação de senha foi enviado para o seu endereço de e-mail.');
-        }
-    });
-}
-
-
-document.getElementById('recoveryForm').addEventListener('submit', function(event) {
+document.getElementById('recoveryForm').addEventListener('submit', async function(event) {
     event.preventDefault();
     
-    // Obter o e-mail digitado pelo usuário
-    const email = document.getElementById('email').value;
     
-    // Chamando a função para enviar o e-mail de recuperação de senha
-    enviarEmailRecuperacaoSenha(email);
+    const email = document.getElementById('email').value;
+    const novaSenha = document.getElementById('novaSenha').value;
+    
+    
+    const requestBody = {
+        email_usuario: email,
+        nova_senha: novaSenha
+    };
+    
+    try {
+        
+        const response = await fetch('https://node-deploy-api-d20r.onrender.com/usuario/trocarsenha', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+        
+        if (response.ok) {
+            
+            document.getElementById('mensagemSucesso').textContent = 'Senha alterada com sucesso!';
+            document.getElementById('mensagemSucesso').classList.remove('hidden');
+            document.getElementById('mensagemErro').classList.add('hidden');
+        } else {
+            
+            document.getElementById('mensagemErro').textContent = 'Erro ao alterar a senha. Por favor, tente novamente.';
+            document.getElementById('mensagemErro').classList.remove('hidden');
+            document.getElementById('mensagemSucesso').classList.add('hidden');
+        }
+    } catch (error) {
+        
+        console.error('Erro ao enviar solicitação:', error);
+        document.getElementById('mensagemErro').textContent = 'Erro ao enviar solicitação. Por favor, tente novamente mais tarde.';
+        document.getElementById('mensagemErro').classList.remove('hidden');
+        document.getElementById('mensagemSucesso').classList.add('hidden');
+    }
 });
