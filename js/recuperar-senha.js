67b2995bafbf506e88d0561c3365c4f8f19404c4
joaothallys <joaothallys>
2024-05-18 12:20:02 -0300

    4

---
+++ js/recuperar-senha.js
@@ -0,0 +1,5 @@
+document.getElementById('recoveryForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Implementar a lógica de recuperação de senha aqui
+    alert('Formulário enviado!');
+});
